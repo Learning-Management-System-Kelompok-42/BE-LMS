2dad96b002a0cd0103688bf5030725209b34ce19
Harly <academyynetworking@gmail.com>
2022-06-08 09:55:19 +0800

    refactor: naming variable & add timeout

--- app/main.go
+++ app/main.go
@@ -8,6 +8,9 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/Learning-Management-System-Kelompok-42/BE-LMS/api"
+	modules "github.com/Learning-Management-System-Kelompok-42/BE-LMS/app/module"
+
 	"github.com/Learning-Management-System-Kelompok-42/BE-LMS/config"
 	"github.com/Learning-Management-System-Kelompok-42/BE-LMS/util"
 	"github.com/labstack/echo/v4"
@@ -15,11 +18,13 @@ import (
 )
 
 func main() {
-	config := config.GetConfig()
+	cfg := config.GetConfig()
 
-	dbConnection := util.NewConnectionDB(config)
+	dbConnection := util.NewConnectionDB(cfg)
 
-	fmt.Println(dbConnection)
+	controllers := modules.RegisterModules(dbConnection)
+
+	timeoutContext := time.Duration(cfg.App.Timeout) * time.Second
 
 	e := echo.New()
 
@@ -27,20 +32,25 @@ func main() {
 		return c.JSON(http.StatusOK, "LMS API")
 	})
 
+	api.RegistrationPath(e, controllers)
+
 	go func() {
-		address := fmt.Sprintf(":%d", config.App.Port)
+		address := fmt.Sprintf(":%d", cfg.App.Port)
 		if err := e.Start(address); err != nil {
 			log.Info("Shutting down the server")
 		}
 	}()
 
+	// Wait for interrupt signal to gracefully shutdown the server with
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
 
+	// close the database connection
 	defer dbConnection.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// timout 30 seconds to shutdown the server
+	ctx, cancel := context.WithTimeout(context.Background(), timeoutContext)
 	defer cancel()
 
 	if err := e.Shutdown(ctx); err != nil {
